3185c33ca0d6cc40cc4a527d6e5f9eb01f50a521
Nehaggarwal <44695839+Nehaggarwal@users.noreply.github.com>
2021-02-09 16:29:28 +0530

    Update

--- IBMCaseStudy/src/test/java/Runner/testrunner.java
+++ IBMCaseStudy/src/test/java/Runner/testrunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= {"Login"},glue= {"StepDefinition"},tags="@ComposeEmail", monochrome=true,
+@CucumberOptions(features= {"Login"},glue= {"StepDefinition"},tags="", monochrome=true,
 dryRun=false,strict=true,
 plugin= {"html:testoutput/report.html","junit:testoutput/cucumber.xml","json:testoutput/cucumber.json"})
 public class testrunner {
